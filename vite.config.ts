1d945bcaafeec6714a6f27edf211edcecd3a717f
Moises Portillo <moyporti15@gmail.com>
2026-01-08 16:29:45 -0600

    realease complete hero 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path"
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: '/professional_portafolio/',
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
